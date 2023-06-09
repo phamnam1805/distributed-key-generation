5a24ac83829efc8d6075ad48c6b19042d2dac515
Nam Phạm <nam.pv1805@gmail.com>
2023-06-09 15:36:28 +0700

    Change file name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Committee, Voter } from "./dkg";
 import { Tree } from "./merkle-tree";
 import MiMC from "./mimc-hash";
 import Pedersen from "./pedersen-hash";
-import Poseidon from "./poseideon-hash";
+import Poseidon from "./poseidon-hash";
 import { Utils } from "./utils";
 
 export { BabyJub, Committee, Voter, Tree, MiMC, Pedersen, Poseidon, Utils };
